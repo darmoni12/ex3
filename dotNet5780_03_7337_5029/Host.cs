d7f7ed56d02b3a276b088fbde7b222fdfb04b786
darmon <1999darmoni@gmail.com>
2019-12-10 09:41:42 +0200

    Add project files.

---
+++ dotNet5780_03_7337_5029/Host.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_03_7337_5029
+{
+    public class Host
+    {
+        public string HostName { get; set; }
+        public List<HostingUnit> Units { get; set; }
+    }
+}
