9ed705221ade3cffc67a8f6dc7278bd73589b186
darmon <1999darmoni@gmail.com>
2019-12-10 12:18:51 +0200

    כמעט מוכן

--- dotNet5780_03_7337_5029/HostingUnitUserControl.xaml.cs
+++ dotNet5780_03_7337_5029/HostingUnitUserControl.xaml.cs
@@ -21,21 +21,64 @@ namespace dotNet5780_03_7337_5029
     public partial class HostingUnitUserControl : UserControl
     {
         public HostingUnit CurrentHostingUnit { get; set; }
-        private Calendar MyCalendar;
+        private Calendar MyCalendar;        int imageIndex;
+        Viewbox vbImage;
+        Image MyImage;
 
         public HostingUnitUserControl(HostingUnit unit)
         {
+            vbImage = new Viewbox();
             InitializeComponent();
+            imageIndex = 0;
+            vbImage.Width = 75;
+            vbImage.Height = 75;
+            vbImage.Stretch = Stretch.Fill;
+            UserControlGrid.Children.Add(vbImage);
+            Grid.SetColumn(vbImage, 2);
+            Grid.SetRow(vbImage, 0);
             CurrentHostingUnit = unit;
+            MyImage = CreateViewImage();
+            vbImage.Child = null;
+            vbImage.Child = MyImage;
             UserControlGrid.DataContext = unit;
             MyCalendar = CreateCalendar();
             vbCalendar.Child = null;//מחיקה מהתצוגה של החלון הקודם
             vbCalendar.Child = MyCalendar;//הצגה של החלון הנוכחי שיצרנו
-            SetBlackOutDates();
-           
-
-
+            SetBlackOutDates();//הוספת הפונקציות לאיוונטים
+            vbImage.MouseUp += vbImage_MouseUp;
+            vbImage.MouseEnter += vbImage_MouseEnter;
+            vbImage.MouseLeave += vbImage_MouseLeave;
         }
+        private void vbImage_MouseLeave(object sender, MouseEventArgs e)
+        {
+            vbImage.Width = 75;
+            vbImage.Height = 75;
+        }
+        private void vbImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            vbImage.Width = this.Width / 3;
+            vbImage.Height = this.Height;
+        }
+        private void vbImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            vbImage.Child = null;
+            imageIndex =
+           (imageIndex + CurrentHostingUnit.Uris.Count - 1) % CurrentHostingUnit.Uris.Count;
+            MyImage = CreateViewImage();
+            vbImage.Child = MyImage;
+        }
+        private Image CreateViewImage()
+        {
+            Image dynamicImage = new Image();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(@CurrentHostingUnit.Uris[imageIndex]);
+            bitmap.EndInit();
+            // Set Image.Source
+            dynamicImage.Source = bitmap;
+            // Add Image to Window
+            return dynamicImage;
+        }
         private Calendar CreateCalendar()
         {
             Calendar MonthlyCalendar = new Calendar();
@@ -57,9 +100,7 @@ namespace dotNet5780_03_7337_5029
         private void btOrder_Click(object sender, RoutedEventArgs e)
         {
             List<DateTime> myList = MyCalendar.SelectedDates.ToList();
-            //var inter = myList.Intersect(CurrentHostingUnit.AllOrders);//בודק אם יש יום מסומן בהזמנה
-            //foreach (var day in inter)
-            //    return;
+            //int x=myList.Intersect(CurrentHostingUnit.AllOrders).Count();//בודק אם יש יום מסומן בהזמנה
             MyCalendar = CreateCalendar();
             vbCalendar.Child = null;
             vbCalendar.Child = MyCalendar;
--- dotNet5780_03_7337_5029/MainWindow.xaml.cs
+++ dotNet5780_03_7337_5029/MainWindow.xaml.cs
@@ -45,6 +45,19 @@ namespace dotNet5780_03_7337_5029
                                 "http://albums.timg.co.il/userfolders/197/2934182/medium/29341822012104215527.jpg?time=0"
                             }
                         },
+                        new HostingUnit()
+                        {
+                            UnitName="צימר",
+                            Rooms=7,
+                            IsSwimmingPool=true,
+                            AllOrders=new List<DateTime>(),
+                            Uris=new List<string>()
+                            {
+                               "https://www.weekendtop.co.il/images/customerimages/25983/gallery/image_25983_b11ee2c6075f4ba99a334b36ed90f5c6.jpg?w=1028&h=510&mode=crop",
+                               "https://static.wixstatic.com/media/a166f9_04c4dbdc3359490e82784844670d1098~mv2_d_4072_2736_s_4_2.jpg/v1/fill/w_468,h_428,al_c,q_80,usm_0.66_1.00_0.01/%D7%A6%D7%99%D7%9E%D7%A8%20%D7%99%D7%95%D7%A8%D7%98%20%D7%91%D7%A9%D7%95%D7%9E%D7%A8%D7%95%D7%9F%20%D7%97%D7%96%D7%99%D7%AA.webp",
+                               "http://tour-yehuda.org.il/uploads/places/706/1453385454.18463.jpg"
+                            }
+                        },
                          new HostingUnit()
                         {
                             UnitName="חדר",
@@ -53,8 +66,9 @@ namespace dotNet5780_03_7337_5029
                             AllOrders=new List<DateTime>(),
                             Uris=new List<string>()
                             {
-                                "https://upload.wikimedia.org/wikipedia/he/2/2c/%D7%A6%D7%99%D7%9E%D7%A8_%D7%91%D7%A6%D7%A4%D7%95%D7%9F.JPG",
-                                "http://albums.timg.co.il/userfolders/197/2934182/medium/29341822012104215527.jpg?time=0"
+                                "http://albums.tapuz.co.il/Albums/2934182/0-1/gnhifmg/29341822012104215524.jpg",
+                                "http://tour-yehuda.org.il/uploads/places/687/1450083580.45503.jpg",
+                                "http://nadlanbagolan.co.il/wp-content/uploads/2013/03/%D7%A6%D7%99%D7%9E%D7%A8%D7%A2%D7%A54-590x360.jpg"
                             }
                         }
                     }
@@ -73,8 +87,21 @@ namespace dotNet5780_03_7337_5029
                             AllOrders=new List<DateTime>(),
                             Uris=new List<string>()
                             {
-                                 "https://upload.wikimedia.org/wikipedia/he/2/2c/%D7%A6%D7%99%D7%9E%D7%A8_%D7%91%D7%A6%D7%A4%D7%95%D7%9F.JPG",
-                                "http://albums.timg.co.il/userfolders/197/2934182/medium/29341822012104215527.jpg?time=0"
+                                "http://albums.timg.co.il/userfolders/197/2934182/29341822012104215529.jpg?time=0",
+                                "https://www.zimmer.co.il/nf1/11234.jpg"
+                            }
+                        },
+                        new HostingUnit()
+                        {
+                            UnitName="חדר מלון",
+                            Rooms=4,
+                            IsSwimmingPool=true,
+                            AllOrders=new List<DateTime>(),
+                            Uris=new List<string>()
+                            {
+                                "https://www.k-etzion.co.il/Pictures/4_20190122155127.234.JPG",
+                                "https://static.wixstatic.com/media/a2125e_fe3e6823af5d441091de9e1804af3451~mv2_d_5578_3719_s_4_2.jpg/v1/fill/w_1600,h_1066,al_c,q_90/file.jpg",
+                                "https://static.wixstatic.com/media/417742_c3a47100d58b4c20aa7a2e7f7156c38f.jpg/v1/fill/w_556,h_512,al_c,q_80,usm_0.66_1.00_0.01/417742_c3a47100d58b4c20aa7a2e7f7156c38f.webp"
                             }
                         },
                          new HostingUnit()
@@ -85,8 +112,9 @@ namespace dotNet5780_03_7337_5029
                             AllOrders=new List<DateTime>(),
                             Uris=new List<string>()
                             {
-                                 "https://upload.wikimedia.org/wikipedia/he/2/2c/%D7%A6%D7%99%D7%9E%D7%A8_%D7%91%D7%A6%D7%A4%D7%95%D7%9F.JPG",
-                                "http://albums.timg.co.il/userfolders/197/2934182/medium/29341822012104215527.jpg?time=0"
+                                "http://albums.timg.co.il/userfolders/197/2934182/medium/2934182201210422318.jpg?time=0",
+                                "http://www.leilotlevanim.co.il/images/Swita/HP_594_386_92.jpg",
+                                "https://www.weekendtop.co.il/images/customerimages/17830/gallery/image_17830_7.jpg?w=1028&h=510&mode=crop"
                             }
                         }
                     }
@@ -104,8 +132,21 @@ namespace dotNet5780_03_7337_5029
                             AllOrders=new List<DateTime>(),
                             Uris=new List<string>()
                             {
-                                 "https://upload.wikimedia.org/wikipedia/he/2/2c/%D7%A6%D7%99%D7%9E%D7%A8_%D7%91%D7%A6%D7%A4%D7%95%D7%9F.JPG",
-                                "http://albums.timg.co.il/userfolders/197/2934182/medium/29341822012104215527.jpg?time=0"
+                                "http://nadlanbagolan.co.il/wp-content/uploads/2013/03/%D7%A6%D7%99%D7%9E%D7%A8-%D7%A2%D7%A5-590x360.jpg",
+                                "http://tour-yehuda.org.il/uploads/places/686/1449486629.13919.jpg",
+                                "https://www.weekendtop.co.il/images/customerimages/21231/gallery/image_21231_44.jpg?w=1028&h=510&mode=crop"
+                            }
+                        },
+                         new HostingUnit()
+                        {
+                            UnitName="צימר בית וגן",
+                            Rooms=4,
+                            IsSwimmingPool=false,
+                            AllOrders=new List<DateTime>(),
+                            Uris=new List<string>()
+                            {
+                                "https://static.wixstatic.com/media/08f675_91cda018fd8c4922bcbc74a403f6d009.jpg/v1/fill/w_567,h_851,al_c,q_90,usm_0.66_1.00_0.01/08f675_91cda018fd8c4922bcbc74a403f6d009.webp",
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwtjQkbtpRr0cTeZg3rA9ag1bNzgERyVMoRk0v_XAQAjvwTMHVeQ&s"
                             }
                         },
                           new HostingUnit()
@@ -116,8 +157,8 @@ namespace dotNet5780_03_7337_5029
                             AllOrders=new List<DateTime>(),
                             Uris=new List<string>()
                             {
-                                 "https://upload.wikimedia.org/wikipedia/he/2/2c/%D7%A6%D7%99%D7%9E%D7%A8_%D7%91%D7%A6%D7%A4%D7%95%D7%9F.JPG",
-                                "http://albums.timg.co.il/userfolders/197/2934182/medium/29341822012104215527.jpg?time=0"
+                                "http://albums.timg.co.il/userfolders/1/0/3370782201261181754.jpg?time=0",
+                                "https://media-cdn.tripadvisor.com/media/photo-s/13/0f/2e/1d/img-20180524-191636-largejpg.jpg"
                             }
                         }
                     }
@@ -145,6 +186,11 @@ namespace dotNet5780_03_7337_5029
                 MainGrid.Children.Add(a);
                 Grid.SetRow(a, i + 1);
             }
-        }
+        }
+
+        private void tbHostName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
